735c9491ac5e10406e885e9748bfbe818ea54009
ashok londhe <ashoklondhe016@gmail.com>
2024-02-12 16:30:23 +0530

    First commit

---
+++ SampleSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleSwiftUIAppApp.swift
+//  SampleSwiftUIApp
+//
+//  Created by Ashok Londhe on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
